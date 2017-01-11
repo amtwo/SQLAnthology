9e92c9945bdca166c34310ba663f5d7adbe28b39
Andy ᴹᴬᴸᴸᴼᴺ (ᴀᴍ²) <amtwo@users.noreply.github.com>
2017-01-10 23:21:28 -0500

    Add files via upload

---
+++ SqlAnthology.sql
@@ -0,0 +1,121 @@
+﻿CREATE USER SQLAnthology WITH PASSWORD = '<strong password>';
+GO
+CREATE SCHEMA anthology AUTHORIZATION SQLAnthology;
+GO
+
+--DROP TABLE anthology.Blog
+CREATE TABLE anthology.Blog(
+	BlogID       int identity(1,1),
+	DisplayName   nvarchar(30) COLLATE Latin1_General_CI_AS,
+	RssUri nvarchar(200) COLLATE Latin1_General_CI_AS,
+	CONSTRAINT PK_anthologyBlog PRIMARY KEY CLUSTERED (BlogID)
+	);
+GO
+SET IDENTITY_INSERT anthology.Blog ON
+INSERT INTO anthology.Blog (BlogID, DisplayName,RssUri)
+VALUES (1,N'Andy Mallon', N'https://www.am2.co/feed/'),
+       (2,N'SentryOne', N'https://blogs.sentryone.com/feed/'),
+	   (3,N'Ken Fisher', N'https://sqlstudies.com/feed/'),
+	   (4,N'dbatools',N'https://dbatools.io/category/announcements/feed/'),
+	   (5,N'dbareports',N'https://dbareports.io/category/announcements/feed/'),
+	   (6,N'Mike Kane',N'http://www.michaelkane.me/feed/'),
+	   (7,N'SQLPerformance',N'https://sqlperformance.com/feed/'),
+	   (8,N'Brent Ozar Unlimited',N'https://www.brentozar.com/blog/feed/'),
+	   (9,N'Brent Ozar',N'https://ozar.me/feed/');
+SET IDENTITY_INSERT anthology.Blog OFF
+GO
+
+--DROP TABLE anthology.BlogAuthor
+CREATE TABLE anthology.BlogAuthor(
+	BlogID      int,
+	AuthorName    nvarchar(50) COLLATE Latin1_General_CI_AS,
+	TwitterHandle nvarchar(30) COLLATE Latin1_General_CI_AS,
+	CONSTRAINT PK_anthologyBlogAuthor PRIMARY KEY CLUSTERED (BlogID,AuthorName)
+	);
+GO
+INSERT INTO anthology.BlogAuthor (BlogID, AuthorName,TwitterHandle)
+VALUES (1,N'Andy', N'@AMtwo 🏳️‍🌈');
+GO
+
+
+CREATE OR ALTER PROCEDURE anthology.BlogList_Get
+AS
+SET NOCOUNT ON
+SELECT BlogName = DisplayName,
+       [Url]    = RssUri
+FROM anthology.Blog;
+GO
+
+--DROP TABLE anthology.Archive
+CREATE TABLE anthology.Archive (
+	PostUrl nvarchar(200) COLLATE Latin1_General_CI_AS,
+	BlogName nvarchar(30) COLLATE Latin1_General_CI_AS,
+	PostTitle  nvarchar(150) COLLATE Latin1_General_CI_AS,
+	PostAuthor nvarchar(50) COLLATE Latin1_General_CI_AS,
+	PostPublishDate datetime2(0),
+	TweetText nvarchar(340) COLLATE Latin1_General_CI_AS, --Make sure we support emoji
+	IsTweeted bit NOT NULL CONSTRAINT DF_anthologyIsTweeted DEFAULT 0,
+	CONSTRAINT PK_anthologyArchive PRIMARY KEY CLUSTERED (PostUrl) WITH (DATA_COMPRESSION=PAGE),
+	);
+GO
+--TweetText is 340 because I'm not storing the shortened URL. Twitter doesn't count the full URL length.
+CREATE INDEX IX_anthologyArchiveUntweeted 
+	ON anthology.Archive (BlogName, PostTitle, PostAuthor, PostUrl)
+	WHERE IsTweeted = 0;
+GO
+
+
+
+CREATE OR ALTER PROCEDURE anthology.Archive_GetNextTweet
+AS
+SET NOCOUNT ON
+DECLARE @TweetText nvarchar(340),
+        @PostURL nvarchar(200)
+SELECT TOP 1 @TweetText = N'[' + BlogName + N']' + N' ✒️ ' + COALESCE(ba.TwitterHandle,a.PostAuthor) 
+						+ NCHAR(10) + NCHAR(13) + PostTitle,
+			 @PostURL = PostUrl
+FROM anthology.Archive a
+LEFT JOIN anthology.Blog b ON b.DisplayName = a.BlogName
+LEFT JOIN anthology.BlogAuthor ba ON ba.BlogID = b.BlogID AND ba.AuthorName = a.PostAuthor
+WHERE IsTweeted = 0
+ORDER BY NEWID();
+
+IF LEN(@TweetText) > 119
+BEGIN
+	SET @TweetText = SUBSTRING(@TweetText,1,115) + N'…' + CHAR(10) + @PostURL
+END;
+ELSE
+BEGIN
+	SET @TweetText = @TweetText + CHAR(10) + @PostURL
+END;
+
+SELECT PostURL= @PostURL,
+       TweetText = @TweetText
+GO
+
+CREATE OR ALTER PROCEDURE anthology.Archive_Upsert
+	@PostUrl nvarchar(200),
+	@BlogName varchar(30) = NULL,
+	@PostTitle nvarchar(150) = NULL,
+	@PostAuthor varchar(50) = NULL,
+	@PostPublishDate datetime2(0) = NULL,
+	@TweetText nvarchar(340) = NULL,
+	@IsTweeted bit = NULL
+AS
+SET NOCOUNT ON
+UPDATE a
+   SET BlogName        = COALESCE(@BlogName,BlogName),
+	   PostTitle       = COALESCE(@PostTitle,PostTitle),
+	   PostAuthor      = COALESCE(@PostAuthor,PostAuthor),
+	   PostPublishDate = COALESCE(@PostPublishDate,PostPublishDate),
+	   TweetText       = COALESCE(@TweetText,TweetText),
+	   IsTweeted       = COALESCE(@IsTweeted,IsTweeted)
+  FROM anthology.Archive a
+ WHERE PostUrl = @PostUrl;
+
+IF @@ROWCOUNT = 0
+BEGIN
+	INSERT INTO anthology.Archive (PostURL, BlogName, PostTitle,PostAuthor, PostPublishDate, TweetText, IsTweeted)
+	SELECT @PostURL, @BlogName, @PostTitle, @PostAuthor, @PostPublishDate, @TweetText, COALESCE(@IsTweeted,0);
+END;
+GO
---
+++ Twitterbot.ps1
@@ -0,0 +1,160 @@
+﻿############################################################################
+#
+# Params
+#
+############################################################################
+
+$sqlInstance = "<SQL Instance>"
+$sqlDatabase = "<database name>"
+$sqlUsername = "SqlAnthology"
+$sqlPassword = "<stong password>"
+
+
+
+
+############################################################################
+#
+# from https://gallery.technet.microsoft.com/Send-Tweets-via-a-72b97964
+#
+############################################################################
+ 
+workflow Send-Tweet {
+    param (
+    [Parameter(Mandatory=$true)][string]$Message
+    )
+
+    InlineScript {      
+        [Reflection.Assembly]::LoadWithPartialName("System.Security")  
+        [Reflection.Assembly]::LoadWithPartialName("System.Net")  
+        
+        $status = [System.Uri]::EscapeDataString($Using:Message);  
+        $oauth_consumer_key = "<consumer key>";  
+        $oauth_consumer_secret = "<consumer secret>";  
+        $oauth_token = "<access token>";  
+        $oauth_token_secret = "<access token secret>";  
+        $oauth_nonce = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes([System.DateTime]::Now.Ticks.ToString()));  
+        $ts = [System.DateTime]::UtcNow - [System.DateTime]::ParseExact("01/01/1970", "dd/MM/yyyy", $null).ToUniversalTime();  
+        $oauth_timestamp = [System.Convert]::ToInt64($ts.TotalSeconds).ToString();  
+  
+        $signature = "POST&";  
+        $signature += [System.Uri]::EscapeDataString("https://api.twitter.com/1.1/statuses/update.json") + "&";  
+        $signature += [System.Uri]::EscapeDataString("oauth_consumer_key=" + $oauth_consumer_key + "&");  
+        $signature += [System.Uri]::EscapeDataString("oauth_nonce=" + $oauth_nonce + "&");   
+        $signature += [System.Uri]::EscapeDataString("oauth_signature_method=HMAC-SHA1&");  
+        $signature += [System.Uri]::EscapeDataString("oauth_timestamp=" + $oauth_timestamp + "&");  
+        $signature += [System.Uri]::EscapeDataString("oauth_token=" + $oauth_token + "&");  
+        $signature += [System.Uri]::EscapeDataString("oauth_version=1.0&");  
+        $signature += [System.Uri]::EscapeDataString("status=" + $status);  
+  
+        $signature_key = [System.Uri]::EscapeDataString($oauth_consumer_secret) + "&" + [System.Uri]::EscapeDataString($oauth_token_secret);  
+  
+        $hmacsha1 = new-object System.Security.Cryptography.HMACSHA1;  
+        $hmacsha1.Key = [System.Text.Encoding]::ASCII.GetBytes($signature_key);  
+        $oauth_signature = [System.Convert]::ToBase64String($hmacsha1.ComputeHash([System.Text.Encoding]::ASCII.GetBytes($signature)));  
+  
+        $oauth_authorization = 'OAuth ';  
+        $oauth_authorization += 'oauth_consumer_key="' + [System.Uri]::EscapeDataString($oauth_consumer_key) + '",';  
+        $oauth_authorization += 'oauth_nonce="' + [System.Uri]::EscapeDataString($oauth_nonce) + '",';  
+        $oauth_authorization += 'oauth_signature="' + [System.Uri]::EscapeDataString($oauth_signature) + '",';  
+        $oauth_authorization += 'oauth_signature_method="HMAC-SHA1",'  
+        $oauth_authorization += 'oauth_timestamp="' + [System.Uri]::EscapeDataString($oauth_timestamp) + '",'  
+        $oauth_authorization += 'oauth_token="' + [System.Uri]::EscapeDataString($oauth_token) + '",';  
+        $oauth_authorization += 'oauth_version="1.0"';  
+    
+        $post_body = [System.Text.Encoding]::ASCII.GetBytes("status=" + $status);   
+        [System.Net.HttpWebRequest] $request = [System.Net.WebRequest]::Create("https://api.twitter.com/1.1/statuses/update.json");  
+        $request.Method = "POST";  
+        $request.Headers.Add("Authorization", $oauth_authorization);  
+        $request.ContentType = "application/x-www-form-urlencoded";  
+        $body = $request.GetRequestStream();  
+        $body.write($post_body, 0, $post_body.length);  
+        $body.flush();  
+        $body.close();  
+        $response = $request.GetResponse();
+    }
+ }
+
+
+Start-Transcript -Path C:\Scripts\SQLAnthology\transcript.txt -Append
+
+
+############################################################################
+#
+# Step 1: Pull recent tweets into the archive
+#
+############################################################################
+$rssList = Invoke-Sqlcmd -ServerInstance $sqlInstance -Database $sqlDatabase -Username $sqlUsername -Password $sqlPassword -Query "EXEC anthology.BlogList_Get"
+
+
+$rssList | ForEach-Object {
+    #Write-Host $_.blogName
+    #Write-Host $_.Url
+
+    $rss = Invoke-WebRequest $_.Url
+
+    if ($rss.StatusCode -ne 200){
+        #This should log an error somewhere
+        Write-Host "Error"
+        Break
+        }
+
+    #This is the content of the RSS feed
+    [xml]$rssXml = $rss.Content
+    $feed = $rssXml.rss.channel
+    
+   
+    # The blog name from the RSS feed sucks sometimes. Use what's I set up in the configuration
+    $blogName = $_.blogName
+    
+    #Loop through all the posts
+    ForEach ($msg in $Feed.Item){
+
+        # post publish date -- We only want to tweet recent posts
+        [datetime]$postPubDate = $msg.pubDate
+
+        # author -- Exact XML element varies by blog platform
+        $postAuthor = $msg.creator.InnerText
+        if ($postAuthor.length -eq 0){
+            $postAuthor = $msg.creator
+            }
+
+
+        # Title
+        $postTitle = $msg.title
+
+        # URL
+        $postUrl = $msg.link
+        
+        $exists = $archive | Where-Object { $_.postUrl -eq $postUrl }
+        
+        # Add to archive if Post from the last 7 days that isn't in the archive
+		if ($postPubDate -gt (Get-Date).AddDays(-7)) {
+            Invoke-Sqlcmd -ServerInstance $sqlInstance -Database $sqlDatabase -Username $sqlUsername -Password $sqlPassword `
+                        -Query "EXEC anthology.Archive_Upsert @PostURL = N'$($postUrl)', @BlogName = N'$($blogName)', @PostTitle = N'$($postTitle)', @PostAuthor = N'$($postAuthor)', @PostPublishDate = N'$($postPubDate)'"
+        }
+     }#EndForEach Tweet
+    
+ }#EndForEach Blog
+
+############################################################################
+#
+# Step 2: Post a single untweeted tweet from the archive 
+#
+############################################################################
+
+# Pick one random untweeted post to be tweeted
+$untweeted = Invoke-Sqlcmd -ServerInstance $sqlInstance -Database $sqlDatabase -Username $sqlUsername -Password $sqlPassword -Query "EXEC anthology.Archive_GetNextTweet"
+$untweeted.TweetText
+
+# Send the tweet
+$null = Send-Tweet $untweeted.TweetText
+
+# Mark this post as tweeted
+Invoke-Sqlcmd -ServerInstance $sqlInstance -Database $sqlDatabase -Username $sqlUsername -Password $sqlPassword -Query "EXEC anthology.Archive_Upsert @PostURL = N'$($untweeted.postUrl)',@TweetText = N'$($untweeted.TweetText)', @IsTweeted = 1"
+
+
+############################################################################
+############################################################################
+
+
+Stop-Transcript
